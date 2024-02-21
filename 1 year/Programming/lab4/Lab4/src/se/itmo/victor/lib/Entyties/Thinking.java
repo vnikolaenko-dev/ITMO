9f6d44415fbf0696dc6f84825071de94b41db516
Victor Nikolaenko <64604542+vnikolaenko-dev@users.noreply.github.com>
2024-02-21 16:54:55 +0300

    Delete 1 year/Programming/lab4/Lab4 directory

--- 1 year/Programming/lab4/Lab4/src/se/itmo/victor/lib/Entyties/Thinking.java
+++
@@ -1,5 +0,0 @@
-package se.itmo.victor.lib.Entyties;
-
-public interface Thinking {
-    void thinkAbout(Thinkable somebody);
-}
--- 1 year/Programming/lab4/Lab4/src/se/itmo/victor/lib/Items/Drinkable.java
+++
@@ -1,6 +0,0 @@
-package se.itmo.victor.lib.Items;
-
-public interface Drinkable {
-    String drink = "кофе";
-    String drink();
-}
